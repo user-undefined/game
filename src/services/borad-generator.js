ae65246186ce9828af1169c1df29d11c8b4edb18
knerushkin <knerushkin@gmail.com>
2020-03-28 16:44:42 +0100

    components and services

---
+++ src/services/borad-generator.js
@@ -0,0 +1,11 @@
+const BOARD_DIM = 5;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generate() {
+    return 'board';
+}
